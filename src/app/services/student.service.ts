f22186e772556cba54c4d015233cb3a348e27e3c
dpowar <devendra.powar@rediffmail.com>
2024-04-18 10:25:30 +0530

    student services

---
+++ src/app/services/student.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { StudentModel } from '../model';
+
+const STUDENTS: StudentModel[] = [
+  {
+    img: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+    id: 's1',
+    name: 'Suraj',
+    age: 32,
+    email: 'test@test.com',
+    number: '12457654'
+  },
+  {
+    img: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+    id: 's2',
+    name: 'Rahul',
+    age: 12,
+    email: 'test@test.com',
+    number: '12457654',
+  },
+  {
+    img: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+    id: 's3',
+    name: 'Pritesh',
+    age: 23,
+    email: 'test@test.com',
+    number: '12457654',
+  },
+  {
+    img: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+    id: 's4',
+    name: 'Ajinkya',
+    age: 38,
+    email: 'test@test.com',
+    number: '12457654',
+  },
+  {
+    img: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+    id: 's5',
+    name: 'Nikhil',
+    age: 18,
+    email: 'test@test.com',
+    number: '12457654',
+  },
+];
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+  private students: StudentModel[] = STUDENTS;
+  constructor() { }
+
+  getStudentList(): StudentModel[] {
+    return this.students;
+  }
+}
--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -2,49 +2,8 @@ import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { StudentModel } from '../model';
 import { FormsModule } from '@angular/forms';
+import { StudentService } from '../services/student.service';
 
-const STUDENTS: StudentModel[] = [
-  {
-    img: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
-    id: 's1',
-    name: 'Suraj',
-    age: 32,
-    email: 'test@test.com',
-    number: '12457654'
-  },
-  {
-    img: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
-    id: 's2',
-    name: 'Rahul',
-    age: 12,
-    email: 'test@test.com',
-    number: '12457654',
-  },
-  {
-    img: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
-    id: 's3',
-    name: 'Pritesh',
-    age: 23,
-    email: 'test@test.com',
-    number: '12457654',
-  },
-  {
-    img: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
-    id: 's4',
-    name: 'Ajinkya',
-    age: 38,
-    email: 'test@test.com',
-    number: '12457654',
-  },
-  {
-    img: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
-    id: 's5',
-    name: 'Nikhil',
-    age: 18,
-    email: 'test@test.com',
-    number: '12457654',
-  },
-];
 
 @Component({
   selector: 'app-student-list',
@@ -54,19 +13,22 @@ const STUDENTS: StudentModel[] = [
   styleUrl: './student-list.component.scss'
 })
 export class StudentListComponent implements OnInit{
-  public students: StudentModel[] = STUDENTS;
+  public students: StudentModel[] = [];
   public filteredStudent!: StudentModel[];
   public showList: boolean = true;
   public searchByName: string = '';
   public isAssending!: boolean;
 
-  constructor() {
+  constructor(private studentService: StudentService) {
     console.log(this.students);
   }
 
   ngOnInit(): void {
+    this.students = this.studentService.getStudentList();
+    console.log(this.students);
     this.searchStudent()
   }
+  
   toggleTable() {
     this.showList = !this.showList
   }
